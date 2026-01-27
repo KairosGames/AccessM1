8f95bdcb500a08451eb05fbd138647c58dae20bd
Ydris <ydris.benmansour@studioblackflag.com>
2026-01-27 13:22:52 +0100

    managerWIP-setWindow

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -1,1 +1,22 @@
-extends Node2D
+class_name game_manager extends Node2D
+
+var currentIndex: int = 0;
+var currentGame: Node2D
+var games : Array[PackedScene] = []
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	pass
+
+func next_game() -> void:
+	if currentIndex == games.size() - 1:
+		get_tree().quit()
+		return
+	
+	var toDelete: Node2D = currentGame
+	currentIndex += 1
+	currentGame = games[currentIndex].instantiate()
+	add_child(currentGame)
+	
