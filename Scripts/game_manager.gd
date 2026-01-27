4ae2f5dce7c2b24e35a5a6141b8df5ca97cf80f7
Ydris <ydris.benmansour@studioblackflag.com>
2026-01-27 13:25:55 +0100

    commit-for-pull

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -1,10 +1,13 @@
 class_name game_manager extends Node2D
 
+@export var games : Array[PackedScene] = []
+
 var currentIndex: int = 0;
 var currentGame: Node2D
-var games : Array[PackedScene] = []
 
 func _ready() -> void:
+	currentGame = games[currentIndex].instantiate()
+	add_child(currentGame)
 	pass
 
 func _process(delta: float) -> void:
