75f55268fe4e89ec95506155c136bde9c7ca38b7
Ahzzak <134094399+Ahzzak@users.noreply.github.com>
2026-01-28 15:59:37 +0100

    UI DE SES MORTS JE TE DETESTE

---
+++ Scenes/car_game_settings.gd
@@ -0,0 +1,18 @@
+extends TabBar
+
+func _on_h_slider_value_changed(value: float) -> void:
+	GlobalSettings.car_timescale = value
+
+
+
+func _on_bebou_mode_toggled(toggled_on: bool) -> void:
+	if toggled_on == true:
+		GlobalSettings.bebou_mode = true
+	else:
+		GlobalSettings.bebou_mode = false
+
+func _on_one_button_mode_toggled(toggled_on: bool) -> void:
+	if toggled_on == true:
+		GlobalSettings.one_button_mode = true
+	else:
+		toggled_on = false
