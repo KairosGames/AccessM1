0e858aa000170a53b1ddd28132a656fb43fe66ae
Xelonn <124406589+Xelonn@users.noreply.github.com>
2026-01-29 14:31:42 +0100

    Inté Son Car
    Have funnnnn

--- Scripts/car_game/track_end.gd
+++ Scripts/car_game/track_end.gd
@@ -3,6 +3,7 @@ extends Area2D
 
 func _physics_process(delta: float) -> void:
 	position.x += -1000 * delta
+	
 
 func _on_body_entered(body: Node2D) -> void:
 	(get_parent().get_parent().get_parent() as GameManager).next_game()
