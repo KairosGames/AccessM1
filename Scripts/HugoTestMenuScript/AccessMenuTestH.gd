0f8bcd4c4aa0bd5d92e3f90c22dd015bf1a3f151
Ahzzak <134094399+Ahzzak@users.noreply.github.com>
2026-01-30 11:15:17 +0100

    push new ui

--- Scripts/HugoTestMenuScript/AccessMenuTestH.gd
+++ Scripts/HugoTestMenuScript/AccessMenuTestH.gd
@@ -2,22 +2,21 @@ extends CanvasLayer
 
 #region declarations
 @onready var bg_1_general = $Control/Bg1_General
-@onready var audio_box_container = $Control/Audio/AudioBoxContainer
-@onready var controlsbox_container = $Control/Controls/ControlsboxContainer
-@onready var acces_container = $Control/Accessibility/AccesContainer
+@onready var audio_box_container = $Control/Audio/HBoxContainer
+@onready var controlsbox_container = $Control/Controls/VBoxContainer
+@onready var acces_container = $Control/Accessibility/TabContainer
 
-@onready var platformer_container = $Control/Accessibility/AccesContainer/Platformer/PlatformerContainer
-@onready var racing_container = $"Control/Accessibility/AccesContainer/Racing Game/RacingContainer"
-@onready var shmup_container = $Control/Accessibility/AccesContainer/Shmup/ShmupContainer
+#@onready var platformer_container = $Control/Accessibility/AccesContainer/Platformer/PlatformerContainer
+#@onready var racing_container = $"Control/Accessibility/AccesContainer/Racing Game/RacingContainer"
+#@onready var shmup_container = $Control/Accessibility/AccesContainer/Shmup/ShmupContainer
 
 @onready var audio_button = $Control/ButtonLeftContainer/Audio
 @onready var general_button = $Control/ButtonLeftContainer/General_button
 @onready var controls = $Control/ButtonLeftContainer/Controls
 @onready var accessibility = $Control/ButtonLeftContainer/Accessibility
-@onready var racing_game = $"Control/Accessibility/AccesContainer/Racing Game"
-@onready var platformer = $Control/Accessibility/AccesContainer/Platformer
-@onready var shmup = $Control/Accessibility/AccesContainer/Shmup
-
+#@onready var racing_game = $"Control/Accessibility/AccesContainer/Racing Game"
+#@onready var platformer = $Control/Accessibility/AccesContainer/Platformer
+#@onready var shmup = $Control/Accessibility/AccesContainer/Shmup
 
 signal On_Current_Button_Changed()
 var current_pressed_button:
@@ -47,24 +46,24 @@ func ShowPopup():
 		accessibility:
 			controlsbox_container.visible = false
 			SetPopupVisible(acces_container, true)
-		racing_game, platformer, shmup:
+		#racing_game, platformer, shmup:
 			# Ensure the parent is visible first!
 			acces_container.visible = true 
 			
 			# Then show the specific sub-container
-			if current_pressed_button == racing_game:
-				SetPopupVisible(racing_container, true)
-			elif current_pressed_button == platformer:
-				SetPopupVisible(platformer_container, true)
-			elif current_pressed_button == shmup:
-				SetPopupVisible(shmup_container, true)
+			#if current_pressed_button == racing_game:
+				#SetPopupVisible(racing_container, true)
+			#elif current_pressed_button == platformer:
+				#SetPopupVisible(platformer_container, true)
+			#elif current_pressed_button == shmup:
+				#SetPopupVisible(shmup_container, true)
 		
 
 func DisableContainers():
 	if actualPopup == null:
 		return
 
-	var sub_containers = [racing_container, platformer_container, shmup_container]
+	#var sub_containers = [racing_container, platformer_container, shmup_container]
 	
 	# 2. Identify if we are moving to a "Main" menu (Audio, General, etc.)
 	var main_buttons = [audio_button, general_button, controls]
@@ -73,12 +72,12 @@ func DisableContainers():
 		# If switching to a main menu, we hide the current popup normally
 		# (This will hide AccesContainer if it was the actualPopup)
 		actualPopup.visible = false
-	else:
+	#else:
 		# 3. If we are staying within the Accessibility branch, 
 		# only hide the sub-containers, NEVER the acces_container itself.
-		for container in sub_containers:
-			if container != null:
-				container.visible = false
+		#for container in sub_containers:
+			#if container != null:
+				#container.visible = false
 
 func SetBgColor(newcolor : Color):
 	if(bg_1_general.color != newcolor):
@@ -114,16 +113,33 @@ func _on_accessibility_pressed():
 	current_pressed_button =  accessibility
 	SetBgColor(Color.BROWN)
 
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("Menu"):
+		if self.visible == false:
+			self.show()
+			Engine.time_scale = 0
+		else:
+			self.hide()
+			Engine.time_scale = 1
+			GlobalSettings.new_settings()
 
-func _on_racing_game_pressed():
-	current_pressed_button =  racing_game
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+func _on_quit_menu_pressed() -> void:
+	self.hide()
+	Engine.time_scale = 1
+	GlobalSettings.new_settings()
+
+#func _on_racing_game_pressed():
+	#current_pressed_button =  racing_game
 
 
 
-func _on_platformer_pressed():
-	current_pressed_button =  platformer
+#func _on_platformer_pressed():
+	#current_pressed_button =  platformer
 
 
 
-func _on_shmup_pressed():
-	current_pressed_button = shmup
+#func _on_shmup_pressed():
+	#current_pressed_button = shmup
---
+++ Scripts/settings_audio.gd
@@ -0,0 +1,23 @@
+extends TabBar
+
+var master_bus
+var stereo
+
+func _ready() -> void:
+	master_bus = AudioServer.get_bus_index("Master")
+	stereo = AudioServer.get_bus_effect(master_bus, 0)
+	AudioServer.set_bus_bypass_effects(master_bus, true)
+
+func _on_mute_all_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		AudioServer.set_bus_mute(master_bus, true)
+	else:
+		AudioServer.set_bus_mute(master_bus, false)
+
+
+
+func _on_mono_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		AudioServer.set_bus_bypass_effects(master_bus, false)
+	else:
+		AudioServer.set_bus_bypass_effects(master_bus, true)
