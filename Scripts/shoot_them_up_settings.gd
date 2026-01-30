76555757d8a8b9e156bef4f879b8faa1eed1da46
GhostofPast <pierric.saillard@edu.univ-fcomte.fr>
2026-01-30 14:05:49 +0100

    UI tweaks

--- Scripts/shoot_them_up_settings.gd
+++ Scripts/shoot_them_up_settings.gd
@@ -3,24 +3,29 @@ extends TabBar
 func _on_invincibility_toggled(toggled_on: bool) -> void:
 	GlobalSettings.invincibility=toggled_on
 	GlobalSettings.new_settings()
+	Engine.time_scale=0
 
 func _on_autofire_toggled(toggled_on: bool) -> void:
 	GlobalSettings.autofire=toggled_on
 	GlobalSettings.new_settings()
+	Engine.time_scale=0
 
 func _on_h_slider_value_changed(value: float) -> void:
 	if value!=GlobalSettings.shmup_timescale:
 		GlobalSettings.shmup_timescale=value
 		GlobalSettings.new_settings()
 		$VBoxContainer2/TimeScale/Label.text="Time Scale "+str(floori(value*100))+"%"
+		Engine.time_scale=0
 
 func _on_player_projectile_speed_toggled(toggled_on: bool) -> void:
 	GlobalSettings.playerProjectileSpeed=toggled_on
 	GlobalSettings.new_settings()
+	Engine.time_scale=0
 
 func _on_enemy_projectile_speed_toggled(toggled_on: bool) -> void:
 	GlobalSettings.enemyProjectileSpeed = toggled_on
 	GlobalSettings.new_settings()
+	Engine.time_scale=0
 
 func _on_dark_background_toggled(toggled_on: bool) -> void:
 	GlobalSettings.darkerBackground=toggled_on
